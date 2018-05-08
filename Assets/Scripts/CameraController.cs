2c241c35fdc317e10ebaccd34068a0c8c567dc0e
Stefan Angelov <ste.angelov.tues19@gmail.com>
2018-05-08 10:04:25 +0300

    Rigidbody

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+    private Vector3 targetPosition;
+    public float speed;
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime); //for different fps
+	}
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,21 +6,47 @@ public class PlayerMove : MonoBehaviour
 {
 
     public float moveSpeed;
+    private Animator ani;
+    private Rigidbody2D myBod;
+    private bool isMoving;
+    private Vector2 lastMove;
 
 	void Start ()
     {
-		
+        ani = GetComponent<Animator>();
+        myBod = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update ()
     {
+        isMoving = false;
 		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myBod.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myBod.velocity.y);
+            isMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         else if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            myBod.velocity = new Vector2(myBod.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            isMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
+
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            myBod.velocity = new Vector2(0f, myBod.velocity.y);
+        }
+        if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            myBod.velocity = new Vector2(myBod.velocity.x, 0f);
+        }
+        ani.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        ani.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        ani.SetBool("IsMoving", isMoving);
+        ani.SetFloat("LastMoveX", lastMove.x);
+        ani.SetFloat("LastMoveY", lastMove.y);
 	}
 }
