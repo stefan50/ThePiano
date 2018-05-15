69b54d7218908cc786eba0ba276464d8454f7fb1
momchil <momchiltues19>
2018-05-15 21:27:38 +0300

    MC and background asset change

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,14 +7,12 @@ public class PlayerMove : MonoBehaviour
 
     public float moveSpeed;
     private Animator ani;
-    private Rigidbody2D myBod;
     private bool isMoving;
     private Vector2 lastMove;
 
 	void Start ()
     {
         ani = GetComponent<Animator>();
-        myBod = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update ()
@@ -22,27 +20,17 @@ public class PlayerMove : MonoBehaviour
         isMoving = false;
 		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
         {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-            myBod.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myBod.velocity.y);
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
             isMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         else if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            myBod.velocity = new Vector2(myBod.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
             isMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
-        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-        {
-            myBod.velocity = new Vector2(0f, myBod.velocity.y);
-        }
-        if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-        {
-            myBod.velocity = new Vector2(myBod.velocity.x, 0f);
-        }
         ani.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         ani.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
         ani.SetBool("IsMoving", isMoving);
